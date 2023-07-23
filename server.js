89f505d26a00c99ea952cb69875fa55377cf4d41
Anshika Agarwal <anshika@Anshikas-MacBook-Pro.local>
2023-07-23 12:25:21 +0530

    Added restaurant api

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const path = require("path");
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://anshii03:Geeks123@cluster0.kx0onpq.mongodb.net/"
